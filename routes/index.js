250d2fd52b4624a55402572c9cbffd3c7e21b1dd
jingechen <jingechen@sohu-inc.com>
2019-09-22 19:45:22 +0800

    add:引入bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json( { title: '首页' });
+  res.render("index", {
+    title:"首页"
+  });
 });
 
 module.exports = router;
